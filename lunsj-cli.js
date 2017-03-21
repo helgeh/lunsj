c62d93180a6bdd95763028af281272767696ac04
Helge Hofstad <helge.hofstad@gmail.com>
2017-03-21 13:35:29 +0100

    change 'day' option to an argument

--- lunsj-cli.js
+++ lunsj-cli.js
@@ -8,17 +8,24 @@ const chalk = require('chalk');
 
 const lunsj = require('./lunsj');
 
+var selectedDay = 'today';
+
 program
-  .version('0.0.2')
+  .version('0.1.0')
   .option('-a, --allergi', 'Include allergenes')
   .option('-c, --clip', 'Copy results to clipboard')
-  .option('-d, --day [day]', 'Choose which day of the week to get [today]', 'today')
   .option('-t, --title [title]', 'Set main title for output')
+  .arguments('[day]')
+  .action(function (day) {
+  	if (day)
+  		selectedDay = day;
+  })
+  .description('Specify [day] or leave empty to get menu for current day (today)')
   .parse(process.argv);
 
 var options = {
 	allergi: program.allergi ? 'allergi' : null,
-	day: program.day,
+	day: selectedDay,
 	title: program.title
 };
 
