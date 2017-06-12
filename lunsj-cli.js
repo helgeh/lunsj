8d75b363df4fd71832d190437d0e3d4a0f187411
Helge Hofstad <helge.hofstad@gmail.com>
2017-06-12 10:25:59 +0200

    new: allways display menu in command line

--- lunsj-cli.js
+++ lunsj-cli.js
@@ -11,17 +11,17 @@ const lunsj = require('./lunsj');
 var selectedDay = 'today';
 
 program
-  .version('0.1.0')
-  .option('-a, --allergi', 'Include allergenes')
-  .option('-c, --clip', 'Copy results to clipboard')
-  .option('-t, --title [title]', 'Set main title for output')
-  .arguments('[day]')
-  .action(function (day) {
-  	if (day)
-  		selectedDay = day;
-  })
-  .description('Specify [day] or leave empty to get menu for current day (today)')
-  .parse(process.argv);
+	.version('0.1.0')
+	.option('-a, --allergi', 'Include allergenes')
+	.option('-c, --clip', 'Copy results to clipboard')
+	.option('-t, --title [title]', 'Set main title for output')
+	.arguments('[day]')
+	.action(function (day) {
+		if (day)
+			selectedDay = day;
+	})
+	.description('Specify [day] or leave empty to get menu for current day (today)')
+	.parse(process.argv);
 
 var options = {
 	allergi: program.allergi ? 'allergi' : null,
@@ -30,13 +30,13 @@ var options = {
 };
 
 lunsj(options).then(function (result) {
-	if (program.clip) {
-		ncp.copy(result);
-		console.log('\n' + chalk.cyan('Menu copied to clipboard!'));
-	}
-	else console.log(
+	console.log(
 		chalk.blue('\n--------\n\n') +
 		chalk.cyan(result) + '\n\n' +
 		chalk.blue('--------\n')
 	);
+	if (program.clip) {
+		ncp.copy(result);
+		console.log('\n' + chalk.cyan('Menu copied to clipboard!'));
+	}
 });
